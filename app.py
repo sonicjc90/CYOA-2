3d7592dccc39e01e77469f2f3b960b22fd313b3d
sonicjc90 <juan.medina@csedge.org>
2023-06-03 11:11:36 -0400

    merged indira code

--- app.py
+++ app.py
@@ -6,36 +6,6 @@ import database-info as db
 
 app = Flask(__name__)
 
-player = input("Please enter your name here ---> ").lower
-
-class Room:
-    def __init__ (self, name, tools, prompt)
-    self.name = name 
-    self.tools = tools
-    self.prompt = prompt
-
-ER = Room("Engine Room", "", "")
-CF = Room("Cafeteria", "", "")
-SP = Room("Spawn Point", "", "")
-CC = Room("Control Center", "", "")
-AR = Room("Airlock", "", "")
-MB = Room("Medbay", "", "")
-GA = Room("General Area", "", "")
-
-
-room_dir =  {
-    "top 1" : "Control Center",
-    "top 2" :  "Cafeteria",
-    "middle1" : "Airlock",
-    "middle2" : "Medbay",
-    "middle3" : "General Area",
-    "bottom1" : "Spawn Point",
-    "bottom2" : "Engine Room",
-}
-
-name = input("Please enter your name :")
-print(name)
-
 @app.route('/', methods= ['GET'])
 def index():
     
