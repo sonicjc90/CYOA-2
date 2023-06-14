6ea69bf64f001ad193576d97129b33645a3ce860
sonicjc90 <juan.medina@csedge.org>
2023-06-14 18:30:04 -0400

    new prompt changes

--- game.py
+++ game.py
@@ -21,31 +21,13 @@ class Room:
 
 class Game:
 
-<<<<<<< HEAD
-    ER = Room("Engine Room", "", "")  
-    # Optional room to enter. Only there for immersiveness.
-    CF = Room("Cafeteria", "", "") 
-    #First room player should be allowed to enter after spawn pt. 
-    #Objs:Pass to airlock(cannot enter airlock w/o pass in inventory)
-    SP = Room("Spawn Point", "", "") 
-    #Player is prompted with a backstory as to why they have randomly woken up here (Could be bedroom instead of spawn pt.)
-    CC = Room("Control Center", "", "") #Last place player should enter, also cannot enter without pass for airlock
-    AR = Room("Airlock", "", "") 
-    #Player will find a key and be prompted to catch it in a short time framed sequence(Sequence starts once audio cue goes off)
-    #If the player fails they die
-    MB = Room("Medbay", "", "") 
-    #Possible threat in this room...If player doesnt have a weapon when dealing w a threat, there is a suicide option
-    GA = Room("General Area", "", "") 
-    #Weapons in this room. Should be the first room players walks into when leaving cafeteria.
-=======
-    ER = Room("Engine Room", "", "", "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/4dd0906b-fdff-4f40-9658-ebf80b146d6c/da7jro9-2ff2e668-a063-43f9-b79e-2c9f4f53e611.jpg/v1/fill/w_1024,h_575,q_75,strp/engine_room_by_kypcaht_da7jro9-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NTc1IiwicGF0aCI6IlwvZlwvNGRkMDkwNmItZmRmZi00ZjQwLTk2NTgtZWJmODBiMTQ2ZDZjXC9kYTdqcm85LTJmZjJlNjY4LWEwNjMtNDNmOS1iNzllLTJjOWY0ZjUzZTYxMS5qcGciLCJ3aWR0aCI6Ijw9MTAyNCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.TET31UwzEsRQBRP2fbohnAeGcdO7Ndznc_mcnb83p7s")
-    CF = Room("Cafeteria", "", "", "https://i.pinimg.com/736x/88/fc/1f/88fc1f76dcd2e315f1a11515a0c815a9--scifi-interior-space-station.jpg")
-    SP = Room("Spawn Point", "", "", "https://i.ytimg.com/vi/qQvep-jJicg/maxresdefault.jpg")
-    CC = Room("Control Center", "", "", "https://media.istockphoto.com/id/1211233381/photo/spaceship-interior-with-view-on-space-and-distant-planets-system-3d-rendering.jpg?s=612x612&w=0&k=20&c=_Dbn5EufB3sUmmCseMGSY2wLX-oJzM6aauG4nfSVB0Y=")
-    AR = Room("Airlock", "", "", "https://i.pinimg.com/originals/69/80/94/698094b03928865aa984eb5602198d3d.jpg")
-    MB = Room("Medbay", "", "", "https://gamestudio.champlain.edu/wp-content/uploads/2021/10/megan-tyler-Sci_Fi_Med_Bay.jpg")
-    GA = Room("General Area", "", "", "https://imagedelivery.net/9sCnq8t6WEGNay0RAQNdvQ/UUID-cl9b8wql61990sfn6y8hzvkfe/public")
->>>>>>> c33d31773f4268548b61eb73afb065c2f730b23e
+    ER = Room("Engine Room", "", "Optional room to enter. Only there for immersiveness.", "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/4dd0906b-fdff-4f40-9658-ebf80b146d6c/da7jro9-2ff2e668-a063-43f9-b79e-2c9f4f53e611.jpg/v1/fill/w_1024,h_575,q_75,strp/engine_room_by_kypcaht_da7jro9-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NTc1IiwicGF0aCI6IlwvZlwvNGRkMDkwNmItZmRmZi00ZjQwLTk2NTgtZWJmODBiMTQ2ZDZjXC9kYTdqcm85LTJmZjJlNjY4LWEwNjMtNDNmOS1iNzllLTJjOWY0ZjUzZTYxMS5qcGciLCJ3aWR0aCI6Ijw9MTAyNCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.TET31UwzEsRQBRP2fbohnAeGcdO7Ndznc_mcnb83p7s")
+    CF = Room("Cafeteria", "", "First room player should be allowed to enter after spawn pt. Objective:Pass to airlock(cannot enter airlock w/o pass in inventory)", "https://i.pinimg.com/736x/88/fc/1f/88fc1f76dcd2e315f1a11515a0c815a9--scifi-interior-space-station.jpg")
+    SP = Room("Spawn Point", "", "Player is prompted with a backstory as to why they have randomly woken up here (Could be bedroom instead of spawn pt.)", "https://i.ytimg.com/vi/qQvep-jJicg/maxresdefault.jpg")
+    CC = Room("Control Center", "", "Last place player should enter, also cannot enter without pass for airlock", "https://media.istockphoto.com/id/1211233381/photo/spaceship-interior-with-view-on-space-and-distant-planets-system-3d-rendering.jpg?s=612x612&w=0&k=20&c=_Dbn5EufB3sUmmCseMGSY2wLX-oJzM6aauG4nfSVB0Y=")
+    AR = Room("Airlock", "", "Player will find a key and be prompted to catch it in a short time framed sequence(Sequence starts once audio cue goes off) If the player fails they die", "https://i.pinimg.com/originals/69/80/94/698094b03928865aa984eb5602198d3d.jpg")
+    MB = Room("Medbay", "", "Possible threat in this room...If player doesnt have a weapon when dealing w a threat, there is a suicide option", "https://gamestudio.champlain.edu/wp-content/uploads/2021/10/megan-tyler-Sci_Fi_Med_Bay.jpg")
+    GA = Room("General Area", "", "Weapons in this room. Should be the first room players walks into when leaving cafeteria.", "https://imagedelivery.net/9sCnq8t6WEGNay0RAQNdvQ/UUID-cl9b8wql61990sfn6y8hzvkfe/public")
     LOCATIONS = [SP, ER, CF, CC, AR, MB, GA]
     
 
